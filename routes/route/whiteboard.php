554953ddd8a18b068b7b6f9fdd2da94c6d815eb9
warmkatahira <t.katahira@warm.co.jp>
2026-06-15 14:27:58 +0900

    20260615_1

--- routes/route/whiteboard.php
+++ routes/route/whiteboard.php
@@ -12,11 +12,11 @@ use App\Http\Controllers\Whiteboard\OrgChart\TextController;
 Route::middleware('common')->group(function () {
     // ホワイトボード一覧
     Route::controller(WhiteboardController::class)->prefix('whiteboard')->name('whiteboard.')->group(function () {
-        Route::get('',                    'index')->name('index');
-        Route::post('',                   'store')->name('store');
-        Route::patch('{whiteboard}',      'update')->name('update');
+        Route::get('', 'index')->name('index');
+        Route::post('', 'store')->name('store');
+        Route::patch('{whiteboard}', 'update')->name('update');
         Route::patch('{whiteboard}/users','updateUsers')->name('update_users');
-        Route::delete('{whiteboard}',     'destroy')->name('destroy');
+        Route::delete('{whiteboard}', 'destroy')->name('destroy');
     });
     // 組織図
     Route::controller(OrgChartController::class)->prefix('org_chart')->name('org_chart.')->group(function () {
@@ -28,18 +28,18 @@ Route::middleware('common')->group(function () {
     });
     // スタッフ
     Route::controller(StaffController::class)->prefix('org_chart')->name('org_chart.')->group(function () {
-        Route::post('staff',           'store')->name('store_staff');
-        Route::patch('staff/{staff}',  'update')->name('update_staff');
+        Route::post('staff', 'store')->name('store_staff');
+        Route::patch('staff/{staff}', 'update')->name('update_staff');
         Route::delete('staff/{staff}', 'destroy')->name('delete_staff');
     });
-    // ゾーン
+    // グループ
     Route::controller(ZoneController::class)->prefix('org_chart')->name('org_chart.')->group(function () {
-        Route::post('zone',          'store')->name('store_zone');
+        Route::post('zone', 'store')->name('store_zone');
         Route::delete('zone/{item}', 'destroy')->name('delete_zone');
     });
     // テキスト
     Route::controller(TextController::class)->prefix('org_chart')->name('org_chart.')->group(function () {
-        Route::post('text',          'store')->name('store_text');
+        Route::post('text', 'store')->name('store_text');
         Route::delete('text/{item}', 'destroy')->name('delete_text');
     });
 });
